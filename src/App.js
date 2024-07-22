53c6570d5d26c4273189132a1ff79b1a9f6fe595
kimyejin <yjin@goatfarm.ai>
2024-07-22 13:50:29 +0900

    js화

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import Signin from "./Signin";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Signin from './Signin';
+import Signup from './Signup';
 
 function App() {
   return (
-    <div className="App">
-      <Signin />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Signin />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
